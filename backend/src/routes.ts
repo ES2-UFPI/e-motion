7887c3d1484d59466affabe58ea0650385ef4600
samuelssan <samuelssan28@gmail.com>
2021-06-15 21:14:21 -0300

    criando rota  para deletar registro de um usuario #61

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ const emotionalReactionController = new EmotionalReactionController();
 
 routes.get("/reactions/:client_id",emotionalReactionController.index);
 
+routes.delete("/reactions/:client_id/:emotional_reaction_id",emotionalReactionController.destroy);
 
 
 export {routes}
