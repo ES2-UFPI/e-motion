1f966a53cbe51768d8d7357d9f9607ccda00ef58
samuelssan <samuelssan28@gmail.com>
2021-06-15 21:09:59 -0300

    criando rota de listar registro de um usuario #58

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,11 @@
 import {Router} from 'express';
+import { EmotionalReactionController } from './controllers/EmotionalRecordController';
 
-const routes = Router();
+const routes = Router(); 
+const emotionalReactionController = new EmotionalReactionController();
 
+
+routes.get("/reactions/:client_id",emotionalReactionController.index);
 
 
 
