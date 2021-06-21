387e8a2aae6a3cca0e7d1a4bd1e830b9a8cd09e9
basilioarth <basilio.arth@gmail.com>
2021-06-21 15:49:16 -0300

    Iniciando a issue #21

---
+++ frontend/src/views/acompanhamento/views/Client/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BackGroundPage } from './styles';
+
+const Client = (props: any) => {
+
+    return (
+        <>
+        <BackGroundPage>
+            
+        </BackGroundPage>
+        </>
+    )
+}
+
+export default Client;
---
+++ frontend/src/views/acompanhamento/views/Client/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+export const BackGroundPage = styled.View`
+    background: #E1948B;
+    flex: 1;
+`
