f184b9a82d512f9a5ec9e4ebf784ad630efcee3a
giselesousar <giselesousar@gmail.com>
2021-06-21 15:36:11 -0300

    add create emotional reaction handler #50

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,18 +1,15 @@
 import {Router} from 'express';
 import { EmotionalReactionController } from './controllers/EmotionalRecordController';
-import { ProfessionalController } from './controllers/ProfessionalController';
 
 const routes = Router(); 
+
 const emotionalReactionController = new EmotionalReactionController();
-const professionalController = new ProfessionalController();
 
 
 routes.get("/reactions/:client_id",emotionalReactionController.index);
 
 routes.delete("/reactions/:client_id/:emotional_reaction_id",emotionalReactionController.destroy);
 
-
-routes.put("/update/:professional_id/:new_association_code",professionalController.update_association_code);
-
+routes.post("/reactions/:client_id",emotionalReactionController.create);
 
 export {routes}
--- backend/src/services/EmotionalReactionService.ts
+++ backend/src/services/EmotionalReactionService.ts
@@ -1,28 +1,35 @@
 import { getRepository, Repository } from 'typeorm';
 import { EmotionalReaction } from '../entities/EmotionalReaction';
 
-class EmotionalReactionService{
-    private emotionalReactionRepository:Repository<EmotionalReaction>;
+class EmotionalReactionService {
+    private emotionalReactionRepository: Repository<EmotionalReaction>;
 
-    constructor(){
+    constructor() {
         this.emotionalReactionRepository = getRepository(EmotionalReaction);
     }
-    
-    async listByUser(client_id:string):Promise<EmotionalReaction[]>{
 
-        const emotionalReactions = await this.emotionalReactionRepository.find({ where:{client_id} });
+    async listByUser(client_id: string): Promise<EmotionalReaction[]> {
+
+        const emotionalReactions = await this.emotionalReactionRepository.find({ where: { client_id } });
 
         return emotionalReactions;
     }
 
-    
-    async delete(client_id:string,emotional_reaction_id:string){
 
-        await this.emotionalReactionRepository.delete({client_id,id:emotional_reaction_id});
+    async delete(client_id: string, emotional_reaction_id: string) {
+
+        await this.emotionalReactionRepository.delete({ client_id, id: emotional_reaction_id });
     }
 
-     
+    async create(client_id: string): Promise<string> {
+        try {
+            return (await this.emotionalReactionRepository.insert({ client_id })).generatedMaps[0].id
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
 }
 
-export {EmotionalReactionService}
+export { EmotionalReactionService }
 
