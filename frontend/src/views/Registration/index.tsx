904501b6da885cbe64219257eb03356c04b68bd5
giselesousar <giselesousar@gmail.com>
2021-06-21 00:06:07 -0300

    finish issue #46

--- frontend/src/views/Registration/index.tsx
+++ frontend/src/views/Registration/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import StepZero from './views/StepZero';
+import StepOne from './views/StepOne';
 
 const StackSettings = createStackNavigator<RoutesList>();
 
@@ -9,10 +10,12 @@ export default function Registration() {
 
         <StackSettings.Navigator screenOptions={{ headerShown: false }}>
             <StackSettings.Screen name="StepZero" component={StepZero} />
+            <StackSettings.Screen name="StepOne" component={StepOne} />
         </StackSettings.Navigator>
     );
 }
 
 type RoutesList = {
     StepZero: undefined
+    StepOne: undefined
 };
