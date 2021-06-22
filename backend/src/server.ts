af65322e41858b4ba2f6a8f2e32c153efc79d18d
giselesousar <giselesousar@gmail.com>
2021-06-21 23:05:06 -0300

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import {routes} from "./routes";
 require('dotenv/config');
 import './database';
 
 const app = express(); 
 
+app.use(cors({
+    origin: '*',
+    credentials: false
+}))
 app.use(express.json());
 app.use(routes);
 
