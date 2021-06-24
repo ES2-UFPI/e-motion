a0edc98ccb38484a3f9e0d45eaa0c3fc33691ea3
samuelssan <42661697+SamuelSSan28@users.noreply.github.com>
2021-06-23 23:35:01 -0300

    adicionando testes de atualização de profissional #87

--- backend/__tests__/Professional.test.ts
+++ backend/__tests__/Professional.test.ts
@@ -2,6 +2,7 @@ import { createConnection, getConnection, getRepository } from "typeorm";
 import { Client } from "../src/entities/Client";
 import { Professional } from "../src/entities/Professional";
 import { User } from "../src/entities/User";
+import { ProfessionalService } from "../src/services/ProfessionalService";
 import { UserService } from "../src/services/UserService";
 
 
@@ -44,4 +45,42 @@ test("store a Professional and fetch it", async () => {
   });
   
   expect(joe.name).toBe("Joe");
+});
+
+
+test("store a Client and update it", async () => {
+
+  const newProfessional = {
+    name: "Joe",
+    email:"joe2@gmail.com",
+    speciality:"psicologia forense",
+    crm_crp:"071.122.811-79",
+    password:"joepsicologia",
+    type:1
+  }
+
+  
+  const userService = new UserService();
+
+  await userService.createUser(newProfessional);
+
+
+  let joe = await getRepository(Professional).findOne({
+      where: {
+        crm_crp:"071.122.811-79"
+      }
+  });
+
+  const professionalService = new ProfessionalService();
+
+  await professionalService.update({id:joe.id, name:"Joe Atualizado",});
+
+  let joe2 = await getRepository(Professional).findOne({
+    where: {
+      id:joe.id
+    }
+});
+
+  
+  expect(joe2.name).toBe("Joe Atualizado");
 });
