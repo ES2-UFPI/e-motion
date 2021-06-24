7168cb192b23896a8229d5d42aaf0372925c3739
samuelssan <42661697+SamuelSSan28@users.noreply.github.com>
2021-06-23 23:15:21 -0300

    Desenvolvendo issue #88
    Metódos para atualizar o usuário do tipo cliente

--- backend/src/services/ClientService.ts
+++ backend/src/services/ClientService.ts
@@ -1,11 +1,21 @@
 import { getRepository, Repository } from 'typeorm';
 import { Client } from "../entities/Client";
+import { UserService } from './UserService';
 
 
 interface ClientInterface{
     name:string;
     phone:string;
     user_id:string;
+}
+
+interface UpdateClientInterface{
+    name?:string;
+    phone?:string;
+    email?:string;
+    password?:string;
+    professional_id?:string;
+    id:string;
 }
 
 
@@ -20,6 +30,28 @@ class ClientService {
         const newClient =  this.clientRepository.create({name,phone,user_id}); 
         
         await this.clientRepository.save(newClient);
+    }
+
+    async update({id,email,password,name,professional_id,phone}:UpdateClientInterface){
+
+        const client =  await this.clientRepository.findOne({where:[{id}],relations:['user']})
+        const client_new_values = {name,professional_id,phone}
+        
+        if(client){
+            await this.clientRepository.save({
+                ...client
+                ,...client_new_values});
+
+            const userService = new UserService();
+            
+            await userService.updateUser({
+                id:client.user_id,
+                email,
+                password
+            })
+        }else
+            throw new Error(`Cliente não encontrado`)
+
     }
 
 }
