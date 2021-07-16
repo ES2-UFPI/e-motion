5ff4280ff44436038f20797bded0895c9d48fbc7
Gisele Ribeiro <giselesousar@gmail.com>
2021-07-16 18:47:03 -0300

    Update ormconfig.ts

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -2,11 +2,12 @@ require('dotenv/config');
 
 module.exports = {
     "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
+    "host": process.env.BD_HOST,
+    "port": process.env.BD_PORT,
     "username": process.env.BD_USERNAME,
     "password": process.env.BD_PASSWORD,
-    "database": "emotion",
+    "database": process.env.BD_NAME,
+    "synchronize": true,
     "migrations":["./src/database/migrations/**.ts"],
     "cli":{
         "migrationsDir":"./src/database/migrations"
