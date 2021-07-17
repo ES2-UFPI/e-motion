18fdcfae128a239ea2573e7d1afa2719e982d185
samuelssan <42661697+SamuelSSan28@users.noreply.github.com>
2021-07-17 19:46:52 -0300

    implementa a issue #124 de forma estatica

--- frontend/src/views/acompanhamento/views/cliente/index.tsx
+++ frontend/src/views/acompanhamento/views/cliente/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { 
     BackGroundPage, 
     RegistersContainer, 
@@ -19,6 +19,37 @@ import {
 import Record_card from '../../../../components/Record_card';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import FeatherIcon from 'react-native-vector-icons/Feather';
+import LineChartComponent from '../../../../components/LineChartComponent';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Dimensions } from 'react-native';
+import RadioButtonsComponent from '../../../../components/RadioButtonsComponent';
+
+
+interface RadioButtons{
+    name:string;
+    isSelected:boolean;
+    onSelect:()=>void;
+}
+
+interface Record{
+    id:string;
+    title:string;
+    date:string;
+    completed:number;
+}
+
+interface RecordGraph{
+    data:string;
+    value:number;
+
+}
+
+interface UserRecordList{
+    records:Record[];
+    graphData:any;
+}
+
+
 
 const Client = (props: any) => {
 
@@ -26,11 +57,40 @@ const Client = (props: any) => {
     const navigation = useNavigation();
     const profilePicture = require('../../../../assets/profile.png');
 
-    function onPressDelete(id:string){}
+    const [loading, setLoading] = useState<boolean>(true);
+    const [clientRegisters, setClientRegisters] = useState();
+    const [clientRegistersFiltered, setClientRegistersFiltered] = useState();
+
     function onPressCard(){
         navigation.navigate('AcompanharComportamento');
     }
 
+    const [filter, setFilter] = useState<RadioButtons[]>([
+        {
+            name:"Dia",
+            isSelected:true,
+            onSelect:() =>{}
+        },
+        {
+            name:"Semana",
+            isSelected:false,
+            onSelect:() => {}
+        } 
+        ,
+        {
+            name:"Mês",
+            isSelected:false,
+            onSelect:() => {}
+        }
+        ,
+        {
+            name:"Tudo",
+            isSelected:false,
+            onSelect:() => {}
+        }       
+    ]);
+
+    
     const staticRegisters = [
         {
             id: "0",
@@ -53,6 +113,12 @@ const Client = (props: any) => {
         {
             id: "3",
             title: "Chateado com fome",
+            date: "23/06/2021 às 14:00",
+            completed: 20
+        },
+        {
+            id: "32",
+            title: "ffff com fome",
             date: "23/06/2021 às 14:00",
             completed: 20
         }
@@ -94,6 +160,8 @@ const Client = (props: any) => {
                     <RegistersBaseText style={{fontSize: 14, lineHeight: 18, fontWeight:'normal',  color: '#292B2D'}}>Registros</RegistersBaseText>
                     <RegistersBaseText style={{fontSize: 11, lineHeight: 14, fontWeight:'bold', color: '#91919F'}}>Total 10</RegistersBaseText>
                 </Info>
+                
+
                 <FlatList
                     data={staticRegisters}
                     keyExtractor={(item) => item.id}
@@ -103,7 +171,6 @@ const Client = (props: any) => {
                         title={item.title} 
                         date={item.date} 
                         completed={item.completed} 
-                        onPressDelete={onPressDelete} 
                         onPress={onPressCard}
                     />
                     }
@@ -111,7 +178,59 @@ const Client = (props: any) => {
                     showsVerticalScrollIndicator={false}
                     contentContainerStyle={{
                         paddingHorizontal:30,
+                        paddingBottom:15,
+                        marginTop:0,
+                        paddingTop:0
                     }}
+                    ListHeaderComponentStyle={{
+                        marginTop:0,
+                        paddingBottom:0,
+                        marginBottom:0
+                    }}
+                    ListHeaderComponent={
+                        <View 
+                            style={{     
+                                alignItems:"center",
+                                justifyContent: 'center',
+                                marginTop:Dimensions.get('window').width/30,
+                                paddingBottom:0,
+                                marginBottom:0,
+                                marginHorizontal:0
+                            }}
+                        >
+                             <View 
+                                style={{
+                                    width:"100%",
+                                    alignItems:"center",
+                                    justifyContent: 'center',
+                                    marginBottom:Dimensions.get('window').width/20,
+                                }}
+                            >
+                                <RadioButtonsComponent radioButtons={filter} />
+                            </View>
+                    
+                            <View 
+                                style={{
+                                    flex:2,
+                                }}
+                            >
+                                
+                            <ScrollView                                    
+                                    contentContainerStyle={{ 
+                                        alignItems:"center", 
+                                        justifyContent: 'center',                
+                                    }}
+                                    horizontal={true}
+                                > 
+                                    <LineChartComponent 
+                                        labels={['January', 'February', 'March', 'April', 'May', 'June']} 
+                                        data={[20, 45, 28, 80, 99, 43]} 
+                                    />
+                                </ScrollView>
+                            </View>
+                            
+                        </View>
+                    }
                 />
             </RegistersContainer>
         </BackGroundPage>
